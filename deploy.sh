cb3afa83edd1fcc182bf8478770512d8865a7b44
giangcsp <doandinhgiang99@gmail.com>
2024-09-16 18:12:44 -0400

    changed all to test parameters

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 export MINTER=$(dfx identity get-principal)
-export REWARD=mxzaz-hqaaa-aaaar-qaada-cai
-export TOKEN=3pplk-2yaaa-aaaag-alqsq-cai
+export REWARD=xzmtv-kaaaa-aaaap-aibra-cai
+export TOKEN=xqpyj-4iaaa-aaaap-aibqq-cai
 export FEE_RECIPIENT=y7qze-p442g-4trdg-6b3vn-f2m3n-2nlvp-htzga-5jojr-jn6zo-fwbaq-3qe
 
 # 30 days
@@ -12,47 +12,47 @@ dfx deploy minepro_staking_backend_30 --network ic --argument "(record {
   lock_time = 2_592_000;
 })"
 
-# 90 days
-dfx deploy minepro_staking_backend_90 --network ic --argument "(record {
-  reward = record { owner = principal \"${REWARD}\" };
-  token = record { owner = principal \"${TOKEN}\" };
-  fee_recipient = record { owner = principal \"${FEE_RECIPIENT}\" };
-  leave_early_fee = 15;
-  lock_time = 7_776_000;
-})"
-
-# 180 days
-dfx deploy minepro_staking_backend_180 --network ic --argument "(record {
-  reward = record { owner = principal \"${REWARD}\" };
-  token = record { owner = principal \"${TOKEN}\" };
-  fee_recipient = record { owner = principal \"${FEE_RECIPIENT}\" };
-  leave_early_fee = 20;
-  lock_time = 15_552_000;
-})"
-
-# 1 year
-dfx deploy minepro_staking_backend_1year --network ic --argument "(record {
-  reward = record { owner = principal \"${REWARD}\" };
-  token = record { owner = principal \"${TOKEN}\" };
-  fee_recipient = record { owner = principal \"${FEE_RECIPIENT}\" };
-  leave_early_fee = 30;
-  lock_time = 31_536_000;
-})"
-
-# 2 years
-dfx deploy minepro_staking_backend_2year --network ic --argument "(record {
-  reward = record { owner = principal \"${REWARD}\" };
-  token = record { owner = principal \"${TOKEN}\" };
-  fee_recipient = record { owner = principal \"${FEE_RECIPIENT}\" };
-  leave_early_fee = 50;
-  lock_time = 63_072_000;
-})"
-
-# 5 years
-dfx deploy minepro_staking_backend_5year --network ic --argument "(record {
-  reward = record { owner = principal \"${REWARD}\" };
-  token = record { owner = principal \"${TOKEN}\" };
-  fee_recipient = record { owner = principal \"${FEE_RECIPIENT}\" };
-  leave_early_fee = 70;
-  lock_time = 157_680_000;
-})"
+# # 90 days
+# dfx deploy minepro_staking_backend_90 --network ic --argument "(record {
+#   reward = record { owner = principal \"${REWARD}\" };
+#   token = record { owner = principal \"${TOKEN}\" };
+#   fee_recipient = record { owner = principal \"${FEE_RECIPIENT}\" };
+#   leave_early_fee = 15;
+#   lock_time = 7_776_000;
+# })"
+#
+# # 180 days
+# dfx deploy minepro_staking_backend_180 --network ic --argument "(record {
+#   reward = record { owner = principal \"${REWARD}\" };
+#   token = record { owner = principal \"${TOKEN}\" };
+#   fee_recipient = record { owner = principal \"${FEE_RECIPIENT}\" };
+#   leave_early_fee = 20;
+#   lock_time = 15_552_000;
+# })"
+#
+# # 1 year
+# dfx deploy minepro_staking_backend_1year --network ic --argument "(record {
+#   reward = record { owner = principal \"${REWARD}\" };
+#   token = record { owner = principal \"${TOKEN}\" };
+#   fee_recipient = record { owner = principal \"${FEE_RECIPIENT}\" };
+#   leave_early_fee = 30;
+#   lock_time = 31_536_000;
+# })"
+#
+# # 2 years
+# dfx deploy minepro_staking_backend_2year --network ic --argument "(record {
+#   reward = record { owner = principal \"${REWARD}\" };
+#   token = record { owner = principal \"${TOKEN}\" };
+#   fee_recipient = record { owner = principal \"${FEE_RECIPIENT}\" };
+#   leave_early_fee = 50;
+#   lock_time = 63_072_000;
+# })"
+#
+# # 5 years
+# dfx deploy minepro_staking_backend_5year --network ic --argument "(record {
+#   reward = record { owner = principal \"${REWARD}\" };
+#   token = record { owner = principal \"${TOKEN}\" };
+#   fee_recipient = record { owner = principal \"${FEE_RECIPIENT}\" };
+#   leave_early_fee = 70;
+#   lock_time = 157_680_000;
+# })"
--- mine_token/deploy.sh
+++ mine_token/deploy.sh
@@ -5,17 +5,36 @@ export ARCHIVE_CONTROLLER=$(dfx identity get-principal)
 export TRIGGER_THRESHOLD=2000
 export CYCLE_FOR_ARCHIVE_CREATION=100000000
 export NUM_OF_BLOCK_TO_ARCHIVE=1000
-export TOKEN_NAME="MinePro"
-export TOKEN_SYMBOL="MINE"
+export TOKEN_NAME="Fake MinePro"
+export TOKEN_SYMBOL="FMINE"
 export MINTER=$(dfx identity get-principal)
 export FEATURE_FLAGS=true
 export NULL_ADDRESS="aaaaa-aa"
 
-# staked token
+# fake MINE
 dfx deploy mine --network ic --argument "(variant {Init =
 record {
   token_symbol = \"${TOKEN_SYMBOL}\";
   token_name = \"${TOKEN_NAME}\";
+  transfer_fee = ${TRANSFER_FEE};
+  metadata = vec {};
+  feature_flags = opt record{icrc2 = ${FEATURE_FLAGS}};
+  minting_account = record { owner = principal \"${NULL_ADDRESS}\" };
+  initial_balances = vec { record { record { owner = principal \"${MINTER}\"; }; ${PRE_MINTED_TOKENS}; }; };
+  initial_mints = vec { record { owner = principal \"${MINTER}\"; ${PRE_MINTED_TOKENS}; } };
+  archive_options = record {
+    num_blocks_to_archive = ${NUM_OF_BLOCK_TO_ARCHIVE};
+    trigger_threshold = ${TRIGGER_THRESHOLD};
+    controller_id = principal \"${ARCHIVE_CONTROLLER}\";
+    cycles_for_archive_creation = opt ${CYCLE_FOR_ARCHIVE_CREATION};
+  };
+}})"
+
+# fake ckBTC
+dfx deploy ckbtc --network ic --argument "(variant {Init =
+record {
+  token_symbol = \"ckBTC\";
+  token_name = \"Fake ckBTC\";
   transfer_fee = ${TRANSFER_FEE};
   metadata = vec {};
   feature_flags = opt record{icrc2 = ${FEATURE_FLAGS}};
